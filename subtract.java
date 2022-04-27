533d3c934dad1ef129f5b054249ea50603cc22b9
a0601 <66411765+a0601@users.noreply.github.com>
2022-04-28 01:41:24 +0530

    Add files via upload

---
+++ subtract.java
@@ -0,0 +1,16 @@
+
+
+class subtract 
+	{
+	public static void main(String[]args)
+		{
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+
+			
+			System.out.println("Subtraction of two numbers"  +a + " " +b + "is:" +(a-b));
+			
+				}
+
+
+	}
